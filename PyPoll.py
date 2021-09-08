7a20c697b6b095a1a1db33f726985ec3bc577597
jmancini96 <jmancini96@knights.ucf.edu>
2021-09-08 19:02:06 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -14,11 +14,34 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#Initialize a total vote counter
+total_votes = 0
+
+#Candidate Options
+candidate_options = []
+
 #Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    #Print the header row 
+    #Read the header row 
     headers = next(file_reader)
-    print(headers)
     
+    #Print each row in the CSV file
+    for row in file_reader:
+        #Add to the total vote count
+        total_votes += 1
+
+        #Print the candidate name from each row
+        candidate_name = row[2]
+
+        #If the candidate does not matching any existing candidate...
+        if candidate_name not in candidate_options:
+            #Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+#Print the total votes
+print(total_votes)
+
+#Print the candidate list
+print(candidate_options)
