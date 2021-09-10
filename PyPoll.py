4422dd0b6728ce58091f8e4b1e262b8c7bf42c8d
jmancini96 <jmancini96@knights.ucf.edu>
2021-09-10 16:51:59 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -45,7 +45,21 @@ with open(file_to_load) as election_data:
 
         #Add a vote to each candidate's count
         candidate_votes[candidate_name] += 1
-    
+
+#Save the results to our text file
+with open(file_to_save, "w") as txt_file:
+        
+    #Print the final vote count
+    election_results = (
+        f"\nElection Results\n"
+        f"------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"------------------------\n")
+    print(election_results, end="")
+
+    #Save the final vote count to the text file
+    txt_file.write(election_results)
+        
     #Determine the percentage of votes for each candidate by looping through the counts
     #Iterate through the candidate list
     for candidate_name in candidate_votes:
@@ -53,8 +67,11 @@ with open(file_to_load) as election_data:
         votes = candidate_votes[candidate_name]
         #Calculate the percentage of votes
         vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
 
         #Determine if the votes are greater than the winning count
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -71,3 +88,4 @@ with open(file_to_load) as election_data:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
